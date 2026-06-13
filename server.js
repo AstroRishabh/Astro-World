452ec7ffb6bbcdde560b2a82e7ab46fa07441ae6
AstroRishabh <rishabhagarwal032003@gmail.com>
2026-06-13 23:05:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/numerology", numerologyRoutes);
+app.use("/api/kundli", kundliRoutes);
 app.use("/api/ai", aiKundliRoutes);
 
 app.use("/api/kundli-milan", kundliMilanRoutes);
